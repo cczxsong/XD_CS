e346241166593936fd83114b3fe7d2b812ae13ca
cczxsong <cczxsong@163.com>
2019-04-14 20:07:58 +0800

    课程作业TASK1

---
+++ 分布式计算/Task1/task1/src/EchoThreadPool/ServerPool.java
@@ -0,0 +1,54 @@
+package EchoThreadPool;
+
+
+import java.io.*;
+import java.net.Socket;
+
+public class ServerPool extends Thread {
+
+    Socket socket = null;
+
+    public ServerPool(Socket socket) {
+        this.socket = socket;
+    }
+    @Override
+    public void run(){
+        InputStream is=null;
+        InputStreamReader isr=null;
+        BufferedReader br=null;
+        OutputStream os=null;
+        PrintWriter pw=null;
+        try {
+            is = socket.getInputStream();
+            isr = new InputStreamReader(is);
+            br = new BufferedReader(isr);
+            os = socket.getOutputStream();
+            pw = new PrintWriter(os);
+            String info=null;
+            while((info=br.readLine())!=null){
+                System.out.println("Message from client:"+info);
+                pw.println(info);
+                pw.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                if(pw!=null)
+                    pw.close();
+                if(os!=null)
+                    os.close();
+                if(br!=null)
+                    br.close();
+                if(isr!=null)
+                    isr.close();
+                if(is!=null)
+                    is.close();
+                if(socket!=null)
+                    socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
