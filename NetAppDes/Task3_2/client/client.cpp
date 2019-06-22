0fd97e47dce53cc635031409df230db03c66000f
cczxsong <cczxsong@163.com>
2019-06-22 15:21:15 +0800

    3

---
+++ NetAppDes/Task3_2/client/client.cpp
@@ -0,0 +1,48 @@
+﻿// client.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+/*
+client.cpp
+author@cczxsong 16030120046 王松
+*/
+#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+#include <windows.h> 
+using namespace std;
+#pragma comment(lib, "WS2_32.lib") 
+int main()
+{
+	WSADATA wsaData;
+	WORD sockVersion = MAKEWORD(2, 2);
+	SOCKET sock = 0;
+	if (WSAStartup(sockVersion, &wsaData) != 0)
+	{
+		cout << "initlization failed!" << endl;
+		exit(0);
+	}
+	sock = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sock == INVALID_SOCKET)
+	{
+		cout << "failed socket!" << endl;
+		return 0;
+	}
+	sockaddr_in sin;
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(4567);
+	sin.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	if (connect(sock, (sockaddr*)&sin, sizeof(sockaddr)) == -1)
+	{
+		cout << "connect failed!" << endl;
+		return 0;
+	}
+	char buffer[256] = "\0";
+	int nRecv = 0;
+	nRecv = recv(sock, buffer, 256, 0);
+	if (nRecv > 0)
+	{
+		buffer[nRecv] = '\0';
+		cout << "reveive data: " << buffer << endl;
+	}
+	closesocket(sock);
+	WSACleanup();
+	system("pause");
+}
---
+++ NetAppDes/Task3_2/task3/server.cpp
@@ -0,0 +1,61 @@
+﻿// server.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+/*
+server.cpp
+author@cczxsong 16030120046 王松
+*/
+#include "pch.h"
+#include <iostream> 
+#include <stdio.h>
+#include <windows.h>
+
+using namespace std;
+#pragma comment(lib, "WS2_32.lib") 
+int main()
+{
+	WSADATA wsaData;
+	WORD sockVersion = MAKEWORD(2, 2);
+	SOCKET sListen = 0;
+	sockaddr_in sin = { 0 };
+	sockaddr_in remoteAddr = { 0 };
+	char szText[] = "This is Sylvester.";
+	int nAddrLen = 0;
+	nAddrLen = sizeof(sockaddr_in);
+	sin.sin_port = htons(4567);
+	sin.sin_family = AF_INET;
+	sin.sin_addr.S_un.S_addr = INADDR_ANY;
+	if (WSAStartup(sockVersion, &wsaData) != 0)
+	{
+		cout << "initlization failed!" << endl;
+		exit(0);
+	}
+	sListen = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (bind(sListen, (LPSOCKADDR)&sin, sizeof(sin)) == SOCKET_ERROR)
+	{
+		cout << "bind failed!" << endl;
+		return 0;
+	}
+	if (listen(sListen, 5) == SOCKET_ERROR)
+	{
+		cout << "listen failed!" << endl;
+		return 0;
+	}
+	SOCKET sClient = INADDR_ANY;
+	int num = 0;
+	while (true)
+	{
+		sClient = accept(sListen, (SOCKADDR*)&remoteAddr, &nAddrLen);
+		if (sClient == INVALID_SOCKET)
+		{
+			cout << "accept failed!" << endl;
+			continue;
+		}
+		else {
+			num++;
+			cout << "NO." << num << " accept successed!" << endl;
+		}
+		send(sClient, szText, strlen(szText), 0);
+		closesocket(sClient);
+	}
+	closesocket(sListen);
+	WSACleanup();
+}
